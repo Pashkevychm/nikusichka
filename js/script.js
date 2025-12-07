415b6a31d3c96691290cbdf544a675c6262e4d9e
olijnikiaksim@gmail.com <olijnikiaksim@gmail.com>
2025-12-07 15:30:09 +0200

    JS

---
+++ js/script.js
@@ -0,0 +1,109 @@
+let products = [
+    {
+        id: 1,
+        name: "iPhone 13",
+        image: "img/apple-iphone-13-01.jpg",
+        description: "Потужний смартфон з чудовою камерою.",
+        type: "phone",
+        price: 28000
+    },
+    {
+        id: 2,
+        name: "MacBook Air",
+        image: "img/1582399075.jpg",
+        description: "Легкий та швидкий ноутбук для роботи.",
+        type: "laptop",
+        price: 45000
+    },
+    {
+        id: 3,
+        name: "Airpods",
+        image: "img/Apple_airpods_pro.jpg",
+        description: "Легкий та зручні наушники для звучання.",
+        type: "airpods",
+        price: 55000
+    },
+
+    {
+        id: 4,
+        name: "poverbank",
+        image: "img/72bedcd4abb7736fc33d0f50c8609146.jpg",
+        description: "Легкий та швидкий повербанк  для заряджання.",
+        type: "poverbank",
+        price: 65000
+    },
+];
+
+let cart = [];
+
+let productsContainer = document.querySelector('.products-div');
+let btnGroup = document.querySelector('.btn-group');
+
+function renderProducts(items) {
+    productsContainer.innerHTML = ""
+    if (items.length == 0) {
+        productsContainer.innerHTML = '<p>Товарів не знайдено</p>'
+        return;
+    }
+
+    items.forEach(function(item) {
+        let productHTML = `
+            <article class="product">
+                <img src="${item.image}" alt="" class="product-img">
+                <h3 class="product-title">${item.name}</h3>
+                <p class="product-description">${item.description}</p>
+                <p class="product-price"><strong>${item.price} грн</strong></p>
+                <button type="button" class="btn btn-primary add-to-cart-btn">До кошику</button>
+            </article>
+        `
+        productsContainer.innerHTML += productHTML
+    })
+}
+
+function applyFilters(categoryType){
+        if (categoryType == "all"){
+            renderProducts(products)
+        } else{
+            let filteredProducts = products.filter(product => product.type ==  categoryType);
+            renderProducts(filteredProducts);
+        }
+}
+
+function addToCart(productId){
+    let product = products.find(p => p.id  == productId);
+    if (product) {
+        cart.push(product);
+        alert("Товар додано"+ product.name)
+        //# Тут  буде збереження в Cookies or localStorage
+    }
+    }
+
+
+ let productsMap = {
+    "Всі": "all",
+    "Телефони": "phone",
+    "Ноутбуки": "laptop",
+    "Повербанки": "poverbank",
+}
+function setupFilterButtons(){
+    for(let button of btnGroup.children){
+        button.addEventListener("click",function(){
+            let category = productsMap[button.innerHTML]
+            applyFilters(category)
+        })
+    }
+}
+
+productsContainer.addEventListener("click",function(event) {
+    if(event.target.classList.contains("add-to-cart-btn")){
+        let productCart = event.target.closest('.product');
+        let productId = parselnt(productCart.dataset.id)
+        addToCart(productId)
+    }
+})
+renderProducts(products)
+setupFilterButtons()
+
+            
+
+
